a94a939fff9a2698bae95f1b9a42725e5fb272b3
moj <moj@bc.com>
2023-02-25 14:13:05 +0330

    add kuber sec stage4

--- integeration-test.sh
+++ integeration-test.sh
@@ -4,7 +4,7 @@
 
 sleep 5s
 
-PORT=$(kubectl -n defualt get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
+PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
 
 echo $PORT
 echo $applicationURL:$PORT/$applicationURL
