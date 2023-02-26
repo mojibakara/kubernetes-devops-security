58c2b815b9982de5a9ba59ce17c5cd5be4764072
moj <moj@bc.com>
2023-02-26 10:59:15 +0330

    add kuber sec stage3

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -7,7 +7,7 @@ kubectl -n default get deployment ${deploymentName} > /dev/null
 
 if [[ $? -ne 0 ]]; then
     echo "deployment ${deploymentName} doesnt exist"
-    kubectl -n default apply -f k8s_deployment_servie.yaml
+    kubectl -n default apply -f k8s_deployment_service.yaml
 else
     echo "deployment ${deploymentName} exist"
     echo "image name - ${imageName}"
