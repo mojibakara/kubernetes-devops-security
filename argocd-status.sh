015f2e671c5989950ac328c583c77db90122166f
moj <moj@bc.com>
2023-03-11 17:25:49 +0330

    test

--- argocd-status.sh
+++ argocd-status.sh
@@ -2,13 +2,16 @@
 
 #argocd_status.sh
 
-output_token=$(curl -k https://167.235.65.82:32263/api/v1/session -d $'{"username":"admin","password":"QYXdF2KS15gZFIJh"}' | cut -d '"' -f4)
+=$(curl -k https://167.235.65.82:32263/api/v1/session -d $'{"username":"admin","password":"QYXdF2KS15gZFIJh"}' | cut -d '"' -f4)
 
 sync_status=$(curl -k https://167.235.65.82:32263/api/v1/applications -H "Authorization: Bearer $output_token" | jq '.["items"][0]["status"]["operationState"]["message"]')
 health_status=$(curl -k https://167.235.65.82:32263/api/v1/applications -H "Authorization: Bearer $output_token" | jq '.["items"][0]["status"]["resources"][0]["health"]["status"]')
 
+echo "$output_token"
+echo "$sync_status"
+echo "$health_status"
 
-if [[ "${sync_status}" -eq "successfully synced (all tasks run)" && "${health_status}" -eq "Healthy" ]]; then
+if [[ "$sync_status" == "successfully synced (all tasks run)" && "$health_status" == "Healthy" ]]; then
     echo "Successs"
     exit 0;
 else
