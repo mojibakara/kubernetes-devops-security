0014e1fad87bc1005f05132edef2f5eb06c9534e
moj <moj@bc.com>
2023-03-12 11:32:11 +0330

    test

--- argocd-status.sh
+++ argocd-status.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 #argocd_status.sh
-sleep 180
-output_token=$(curl -k https://167.235.65.82:32263/api/v1/session -d $'{"username":"admin","password":"QYXdF2KS15gZFIJh"}' | cut -d '"' -f4)
+sleep 300
 output_token=$(curl -k https://167.235.65.82:32263/api/v1/session -d $'{"username":"admin","password":"QYXdF2KS15gZFIJh"}' | cut -d '"' -f4)
 
-app_sync=$(curl -k https://167.235.65.82:32263/api/v1/applications/myapp/sync -H "Authorization: Bearer $output_token")
-sleep 120
+app_sync=$(curl -k https://167.235.65.82:32263/api/v1/session -d $'{"username":"admin","password":"QYXdF2KS15gZFIJh"}' | cut -d '"' -f4)
+
+sleep 120s
+sync_status=$(curl -k https://167.235.65.82:32263/api/v1/applications/myapp/sync -H "Authorization: Bearer $output_token" | jq '.["items"][0]["status"]["operationState"]["message"]' | sed 's/"//' | sed 's/"//')
 health_status=$(curl -k https://167.235.65.82:32263/api/v1/applications -H "Authorization: Bearer $output_token" | jq '.["items"][0]["status"]["resources"][0]["health"]["status"]' | sed 's/"//' | sed 's/"//')
 
 echo $output_token
