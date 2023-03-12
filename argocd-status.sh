bab6ee4cb022dff9e84608263a8c8f23d063a028
moj <moj@bc.com>
2023-03-12 10:48:08 +0330

    test

--- argocd-status.sh
+++ argocd-status.sh
@@ -4,12 +4,12 @@
 
 output_token=$(curl -k https://167.235.65.82:32263/api/v1/session -d $'{"username":"admin","password":"QYXdF2KS15gZFIJh"}' | cut -d '"' -f4)
 
-sync_status=$(curl -k https://167.235.65.82:32263/api/v1/applications -H "Authorization: Bearer $output_token" | jq '.["items"][0]["status"]["operationState"]["message"]')
-health_status=$(curl -k https://167.235.65.82:32263/api/v1/applications -H "Authorization: Bearer $output_token" | jq '.["items"][0]["status"]["resources"][0]["health"]["status"]')
+sync_status=$(curl -k https://167.235.65.82:32263/api/v1/applications -H "Authorization: Bearer $output_token" | jq '.["items"][0]["status"]["operationState"]["message"]' | sed 's/"//' | sed 's/"//')
+health_status=$(curl -k https://167.235.65.82:32263/api/v1/applications -H "Authorization: Bearer $output_token" | jq '.["items"][0]["status"]["resources"][0]["health"]["status"]' | sed 's/"//' | sed 's/"//')
 
-echo "$output_token"
-echo "$sync_status"
-echo "$health_status"
+echo $output_token
+echo $sync_status
+echo $health_status
 
 if [[ "$sync_status" == "successfully synced (all tasks run)" && "$health_status" == "Healthy" ]]; then
     echo "Successs"
