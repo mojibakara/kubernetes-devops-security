14b1a3b70f2ddbd8cc402a1856bbf3e7801ee42b
moj <moj@bc.com>
2023-03-12 11:11:32 +0330

    test

--- argocd-status.sh
+++ argocd-status.sh
@@ -3,8 +3,9 @@
 #argocd_status.sh
 sleep 150
 output_token=$(curl -k https://167.235.65.82:32263/api/v1/session -d $'{"username":"admin","password":"QYXdF2KS15gZFIJh"}' | cut -d '"' -f4)
+output_token=$(curl -k https://167.235.65.82:32263/api/v1/session -d $'{"username":"admin","password":"QYXdF2KS15gZFIJh"}' | cut -d '"' -f4)
 
-sync_status=$(curl -k https://167.235.65.82:32263/api/v1/applications -H "Authorization: Bearer $output_token" | jq '.["items"][0]["status"]["operationState"]["message"]' | sed 's/"//' | sed 's/"//')
+app_sync=$(curl -k https://167.235.65.82:32263/api/v1/applications/myapp/sync -H "Authorization: Bearer $output_token")
 health_status=$(curl -k https://167.235.65.82:32263/api/v1/applications -H "Authorization: Bearer $output_token" | jq '.["items"][0]["status"]["resources"][0]["health"]["status"]' | sed 's/"//' | sed 's/"//')
 
 echo $output_token
